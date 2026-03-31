6241bb253f73742714bfe11eae864555bc4ddbd2
Milton-Luna <miltonluna.m.16@gmail.com>
2026-03-30 23:12:05 -0500

    Implementacion de autenticacion JWT para el login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,28 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUsuarioDto } from 'src/users/user/dto/create-usuario.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('register')
-    @ApiOperation({ summary: 'Registrar un nuevo usuario' })
-    @ApiResponse({ status: 201, description: 'Usuario registrado exitosamente.' })
-    @ApiResponse({ status: 400, description: 'Error al registrar el usuario.' })
-    async registerUser(@Body() user: CreateUsuarioDto) {
+  @Post('register')
+  @ApiOperation({ summary: 'Registrar un nuevo usuario' })
+  @ApiResponse({ status: 201, description: 'Usuario registrado exitosamente.' })
+  @ApiResponse({ status: 400, description: 'Error al registrar el usuario.' })
+  async registerUser(@Body() user: CreateUsuarioDto) {
     return this.authService.registerUser(user);
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: 'Iniciar sesión' })
+  @ApiResponse({ status: 200, description: 'Inicio de sesión exitoso.' })
+  @ApiResponse({ status: 401, description: 'Credenciales incorrectas.' })
+  async login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    return this.authService.login(email, password);
   }
 }
