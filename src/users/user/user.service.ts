53779786a133c4c0240959d80b31c2e363fb0385
Milton-Luna <miltonluna.m.16@gmail.com>
2026-04-11 21:04:34 -0500

    Gestion de usuarios

--- src/users/user/user.service.ts
+++ src/users/user/user.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { Rol } from '../rol/entity/rol.entity';
+import { Perfil } from '../perfil/entity/perfil.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usuarioRepository: Repository<User>,
+    @InjectRepository(Perfil)
+    private readonly perfilRepository: Repository<Perfil>,
+  ) {}
+
+  async findAll() {
+    return this.usuarioRepository.find({
+      relations: ['rol', 'perfil'],
+      select: {
+        id_usuario: true,
+        email: true,
+        rol: { id: true, nombre: true },
+        perfil: {
+          nombre: true,
+          foto_perfil: true,
+        },
+        isActive: true,
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const usuario = await this.usuarioRepository.findOne({
+      where: { id_usuario: id },
+      relations: ['rol', 'perfil'],
+      select: {
+        id_usuario: true,
+        email: true,
+        rol: { id: true, nombre: true },
+        perfil: {
+          nombre: true,
+          foto_perfil: true,
+        },
+      },
+    });
+
+    if (!usuario) throw new NotFoundException('Usuario no encontrado');
+    return usuario;
+  }
+
+  async toggleStatus(id: number, isActive: boolean) {
+    const usuario = await this.usuarioRepository.findOne({
+      where: { id_usuario: id },
+      relations: ['perfil'],
+    });
+
+    if (!usuario) throw new NotFoundException('Usuario no encontrado');
+    usuario.isActive = isActive;
+    await this.usuarioRepository.save(usuario);
+
+    if (usuario.perfil) {
+      usuario.perfil.isActive = isActive;
+      await this.perfilRepository.save(usuario.perfil);
+    }
+
+    return {
+      message: `Usuario ${isActive ? 'activado' : 'desactivado'} con éxito`,
+      status: isActive,
+    };
+  }
+}
