da1a87009acdbbb9077e02b2dd067569960e07cd
Milton-Luna <miltonluna.m.16@gmail.com>
2026-04-18 21:20:08 -0500

    crud tags con validaciones

---
+++ src/users/perfil/entity/perfil.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { User } from 'src/users/user/entity/user.entity';
+import { Genero } from 'src/users/genero/entity/genero.entity';
+
+@Entity('perfil')
+export class Perfil {
+  @PrimaryGeneratedColumn()
+  id_perfil: number;
+
+  @Column({ length: 150, nullable: true })
+  nombre: string;
+
+  @Column({ length: 255, nullable: true })
+  foto_perfil: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @OneToOne(() => User, (user) => user.perfil, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'id_usuario' })
+  user: User;
+
+  @ManyToOne(() => Genero, (genero) => genero.perfiles, { nullable: true })
+  @JoinColumn({ name: 'id_genero' })
+  genero: Genero;
+}
